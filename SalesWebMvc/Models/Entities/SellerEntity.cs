40439505596505cb6f0b36848b930d8b92752748
leandroliveirac <leandroparipe@hotmail.com>
2023-01-23 22:39:11 -0300

    alterações basicas

--- SalesWebMvc/Models/Entities/SellerEntity.cs
+++ SalesWebMvc/Models/Entities/SellerEntity.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace SalesWebMvc.Models.Entities
 {
     public class SellerEntity : BaseEntity
     {
+        [Required(ErrorMessage = "Obrigatório")]
         public string Name { get; set; }
+        
         public string Email { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Display(Name ="Birth Date")]
         public DateTime BirthDate { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Display(Name = "Base Salary")]
         public double BaseSalary { get; set; }
+        [Display(Name = "Department")]
         public int DepartmentId { get; set; }
         public virtual DepartmentEntity Department { get; set; }
         public virtual ICollection<SalesRecordEntity> SalesRecords { get; set; } = new List<SalesRecordEntity>();
